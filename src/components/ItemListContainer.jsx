f1c4365b461748bbf534c74fee112b93a3659d85
FacuVasquez <Facuvasquez@outlook.com>
2024-02-15 23:08:59 -0300

    Segundo arreglo de rutas

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,7 +3,7 @@ const ItemListContainer = ({ greeting }) => {
   return (
     <div className="item-list-container">
       <h2>{greeting}</h2>
-      {/* Puedes agregar más contenido aquí */}
+      {}
     </div>
   );
 };
